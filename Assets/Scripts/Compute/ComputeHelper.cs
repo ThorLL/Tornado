861def655d9fb1feb3df99675c102dece2e1f2b2
ThorLL <thorlind030601@gmail.com>
2025-06-08 20:09:42 +0200

    Created Unity project

---
+++ Assets/Scripts/Compute/ComputeHelper.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+
+namespace Compute
+{
+    public static class ComputeHelper
+    {
+        static readonly uint[] ArgsBufferArray = new uint[5];
+        
+        public static RenderTexture CreateRenderTexture(int width, int height, FilterMode filterMode, GraphicsFormat format, string name, int depth = 0, bool useMipMaps = false)
+        {
+            RenderTexture texture = new(width, height, depth)
+            {
+                graphicsFormat = format,
+                enableRandomWrite = true,
+                autoGenerateMips = false,
+                useMipMap = useMipMaps
+            };
+            texture.Create();
+
+            texture.name = name;
+            texture.wrapMode = TextureWrapMode.Clamp;
+            texture.filterMode = filterMode;
+            return texture;
+        }
+
+        public static ComputeBuffer CreateStructuredBuffer<T>(int capacity) => new(capacity, GetStride<T>());
+
+        public static ComputeBuffer CreateStructuredBuffer<T>(IEnumerable<T> data)
+        {
+            ComputeBuffer buffer = new(data.Count(), GetStride<T>());
+            buffer.SetData(data.ToArray());
+            return buffer;
+        }
+
+        public static void CreateArgsBuffer(ref ComputeBuffer argsBuffer, Mesh mesh, int numInstances)
+        {
+            const int stride = sizeof(uint);
+            const int numArgs = 5;
+            const int subMeshIndex = 0;
+
+            bool createNewBuffer = argsBuffer == null || !argsBuffer.IsValid() || argsBuffer.count != ArgsBufferArray.Length || argsBuffer.stride != stride;
+            if (createNewBuffer)
+            {
+                argsBuffer?.Release();
+                argsBuffer = new ComputeBuffer(numArgs, stride, ComputeBufferType.IndirectArguments);
+            }
+
+            lock (ArgsBufferArray)
+            {
+                ArgsBufferArray[0] = mesh.GetIndexCount(subMeshIndex);
+                ArgsBufferArray[1] = (uint)numInstances;
+                ArgsBufferArray[2] = mesh.GetIndexStart(subMeshIndex);
+                ArgsBufferArray[3] = mesh.GetBaseVertex(subMeshIndex);
+                ArgsBufferArray[4] = 0; // offset
+				
+                argsBuffer.SetData(ArgsBufferArray);
+            }
+        }
+        
+        static int GetStride<T>() => System.Runtime.InteropServices.Marshal.SizeOf(typeof(T));
+
+        public static void LoadShader(ref ComputeShader computeShader, string resourcePath)
+        {
+            if (computeShader == null) computeShader = (ComputeShader)Resources.Load(resourcePath);
+        }
+    }
+}
---
+++ Assets/Scripts/Compute/Util/Math.hlsl
@@ -0,0 +1,55 @@
+﻿static const float Epsilon = 1.401298E-45f;
+static const float MaxFloat = 3.40282347E+38f;
+static const float MinFloat = -3.40282347E+38f;
+
+static const float PI_HALF = 1.5707963268f;
+static const float PI = 3.1415926536f;
+static const float PI_ONE_HALF = 4.7123889804f;
+static const float PI2 = 6.2831853072f;
+
+static const float sqrt2 = 1.4142135624f;
+static const float halfSqrt2 = 0.7071067812f;
+
+// Modulo 289 without a division (only multiplications)
+static float  mod289(float x)  { return x - floor(x * (1.0f / 289.0f)) * 289.0f; }
+static float2 mod289(float2 x) { return x - floor(x * (1.0f / 289.0f)) * 289.0f; }
+static float3 mod289(float3 x) { return x - floor(x * (1.0f / 289.0f)) * 289.0f; }
+static float4 mod289(float4 x) { return x - floor(x * (1.0f / 289.0f)) * 289.0f; }
+
+// Modulo 7 without a division
+static float  mod7(float x)  { return x - floor(x * (1.0f / 7.0f)) * 7.0f; }
+static float2 mod7(float2 x) { return x - floor(x * (1.0f / 7.0f)) * 7.0f; }
+static float3 mod7(float3 x) { return x - floor(x * (1.0f / 7.0f)) * 7.0f; }
+static float4 mod7(float4 x) { return x - floor(x * (1.0f / 7.0f)) * 7.0f; }
+
+// Permutation polynomial: (34x^2 + x) math.mod 289
+static float  permute(float x)  { return mod289((34.0f * x + 1.0f) * x); }
+static float2 permute(float2 x) { return mod289((34.0f * x + 1.0f) * x); }
+static float3 permute(float3 x) { return mod289((34.0f * x + 1.0f) * x); }
+static float4 permute(float4 x) { return mod289((34.0f * x + 1.0f) * x); }
+
+static float  taylorInvSqrt(float r)  { return 1.79284291400159f - 0.85373472095314f * r; }
+static float2 taylorInvSqrt(float2 r) { return 1.79284291400159f - 0.85373472095314f * r; }
+static float3 taylorInvSqrt(float3 r) { return 1.79284291400159f - 0.85373472095314f * r; }
+static float4 taylorInvSqrt(float4 r) { return 1.79284291400159f - 0.85373472095314f * r; }
+
+static uint NextState(inout uint state)
+{
+    state ^= state << 13;
+    state ^= state >> 17;
+    state ^= state << 5;
+    return state;
+}
+
+static float NextFloat(inout uint state)
+{
+    return asfloat(0x3f800000 | (NextState(state) >> 9)) - 1.0f;
+}
+
+static float2 NextFloat2Direction(inout uint state)
+{
+    float angle = NextFloat(state) * PI * 2.0f;
+    float s, c;
+    sincos(angle, s, c);
+    return float2(c, s);
+}
