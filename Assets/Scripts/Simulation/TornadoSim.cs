884f31901b0ce6272c44dac339cf8f907a0b031f
ThorLL <thorlind030601@gmail.com>
2025-06-10 23:18:03 +0200

    Particle system foundation

---
+++ Assets/Scripts/Simulation/TornadoSim.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using Compute;
+using Unity.Mathematics;
+
+namespace Simulation
+{
+    public class TornadoSim : MonoBehaviour
+    {
+        [Header("References")]
+        public Spawner spawner;
+        ComputeShader _simulation;
+
+        public ComputeBuffer PositionBuffer;
+        public ComputeBuffer VelocityBuffer;
+        
+        void Start()
+        {
+
+            ComputeHelper.LoadShader(ref _simulation, "TornadoSim");
+            Initialise();
+        }
+
+        void Initialise()
+        {
+            if (!spawner) return;
+
+            PositionBuffer?.Release();
+            VelocityBuffer?.Release();
+            
+            (float3[] points, float3[] velocities) = spawner.GetSpawnData();
+            PositionBuffer = ComputeHelper.CreateStructuredBuffer(points);
+            VelocityBuffer = ComputeHelper.CreateStructuredBuffer(velocities);
+
+            _simulation.SetBuffer(0, "Positions", PositionBuffer);
+            _simulation.SetBuffer(0, "Velocities", VelocityBuffer);
+            
+            _simulation.SetInt("numParticles", PositionBuffer.count);
+        }
+
+        void OnDestroy()
+        {
+            PositionBuffer.Release();
+            VelocityBuffer.Release();
+        }
+    }
+}
